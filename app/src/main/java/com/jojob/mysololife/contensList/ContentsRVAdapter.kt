1ebc5f07bfcdf75c4502ebdcf0191863e36e187a
Jaehun-Jo <chojh922@gmail.com>
2021-07-30 00:25:07 +0900

    RecyclerView 수정1

--- app/src/main/java/com/jojob/mysololife/contensList/ContentsRVAdapter.kt
+++ app/src/main/java/com/jojob/mysololife/contensList/ContentsRVAdapter.kt
@@ -1,18 +1,36 @@
 package com.jojob.mysololife.contensList
 
+import android.content.Context
+import android.media.Image
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.jojob.mysololife.R
 
-class ContentsRVAdapter (val items : ArrayList<String>) : RecyclerView.Adapter<ContentsRVAdapter.Viewholder>() {
+class ContentsRVAdapter (val context : Context, val items : ArrayList<ContentModel>) : RecyclerView.Adapter<ContentsRVAdapter.Viewholder>() {
+
+    interface ItemClick {
+        fun onClick (view: View, position: Int)
+    }
+
+    var itemClick : ItemClick? = null
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContentsRVAdapter.Viewholder {
         val v = LayoutInflater.from(parent.context).inflate(R.layout.contents_rv_item, parent, false)
         return Viewholder(v)
     }
 
     override fun onBindViewHolder(holder: ContentsRVAdapter.Viewholder, position: Int) {
+        if(itemClick != null) {
+            holder.itemView.setOnClickListener { v->
+                itemClick?.onClick(v, position)
+            }
+        }
+
         holder.bindItems(items[position])
     }
 
@@ -22,8 +40,14 @@ class ContentsRVAdapter (val items : ArrayList<String>) : RecyclerView.Adapter<C
 
     inner class Viewholder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-        fun bindItems(item : String) {
+        fun bindItems(item : ContentModel) {
+            val contentTitle = itemView.findViewById<TextView>(R.id.textArea)
+            contentTitle.text = item.title
 
+            val imageViewArea = itemView.findViewById<ImageView>(R.id.imageArea)
+            Glide.with(context)
+                .load(item.imageUrl)
+                .into(imageViewArea)
         }
 
     }
